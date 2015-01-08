a72fb25a273438f8e6143c885e1d3af141a444d4
mklitgaard <mtklitgaard@gmail.com>
2015-01-07 21:23:49 -0700

    Add untracked files

---
+++ ComicLookup/ComicLookup.Web.Tests/CharacterControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using ComicLookup.Controllers;
+using ComicLookup.Domain;
+using NUnit.Framework;
+
+namespace ComicLookup.Web.Tests
+{
+    public class CharacterControllerTests
+    {
+        private CharacterController _classUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _classUnderTest = new CharacterController();
+        }
+
+        public class Name : CharacterControllerTests
+        {
+            [Test]
+            public void ReturnsResponseWithCharacter_WhenNameIsNotNullOrWhiteSpace()
+            {
+                var name = "Hulk";
+                var actual = _classUnderTest.Name(name);
+                Assert.That(actual, Is.TypeOf(typeof(ResponseEnvelope<Character>)));
+                Assert.That(actual.Result, Is.TypeOf(typeof(Character)));
+            }
+        }
+    }
+}
