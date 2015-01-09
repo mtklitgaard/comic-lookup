e6634bdcce634d205b7ed2ca76ecf1185692792b
mklitgaard <mtklitgaard@gmail.com>
2015-01-08 21:44:45 -0700

    Remove class

---
+++ ComicLookup/ComicLookup.Services.Tests/CharacterBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using ComicLookup.Services.Builders;
+using NUnit.Framework;
+
+namespace ComicLookup.Services.Tests
+{
+    public class CharacterBuilderTests
+    {
+        private CharacterBuilder _classUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _classUnderTest = new CharacterBuilder();
+        }
+
+        [TestFixture]
+        public class GetCharacterByName : CharacterBuilderTests
+        {
+           
+
+        }
+    }
+}
