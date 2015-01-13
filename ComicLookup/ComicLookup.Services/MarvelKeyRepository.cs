b76d42c4f5384d9d9df185c2bb3e3c0c66c00b07
mklitgaard <mtklitgaard@gmail.com>
2015-01-12 21:33:32 -0700

    Add key repo

---
+++ ComicLookup/ComicLookup.Services/MarvelKeyRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using ComicLookup.Services.Interfaces;
+
+namespace ComicLookup.Services
+{
+    public class MarvelKeyRepository : IMarvelKeyRepository
+    {
+        public string ApiKey { get; private set; }
+        public string TimeStamp { get; private set; }
+        public Uri MarvelBaseUrl { get; private set; }
+        public string Hash()
+        {
+            //Input secret key building here
+        }
+    }
+}
