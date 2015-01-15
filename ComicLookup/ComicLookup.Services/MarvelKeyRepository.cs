561f8b9a51269ae20d77448933b713b5934febf3
mklitgaard <mtklitgaard@gmail.com>
2015-01-14 20:10:56 -0700

    Update url generating

--- ComicLookup/ComicLookup.Services/MarvelKeyRepository.cs
+++ ComicLookup/ComicLookup.Services/MarvelKeyRepository.cs
@@ -1,16 +1,36 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
 using ComicLookup.Services.Interfaces;
 
 namespace ComicLookup.Services
 {
     public class MarvelKeyRepository : IMarvelKeyRepository
     {
-        public string ApiKey { get; private set; }
+        const string PRIVATE_KEY = "PrivateKey"; 
+
+        public string ApiKey { get { return "APIKEY"; } }
         public string TimeStamp { get; private set; }
-        public Uri MarvelBaseUrl { get; private set; }
+        public Uri MarvelBaseUrl { get { return new Uri("https://gateway.marvel.com/"); } }
+
+        public MarvelKeyRepository()
+        {
+            TimeStamp = DateTime.Now.ToString();
+        }
+
         public string Hash()
-        { 
-            //Input secret key building here
+        {
+            var hash = new StringBuilder();
+            using (MD5 md5 = MD5.Create())
+            {
+                var md5Results = md5.ComputeHash(Encoding.UTF8.GetBytes(TimeStamp+PRIVATE_KEY+ApiKey));
+
+                for (int i = 0; i < md5Results.Length; i++)
+                {
+                    hash.Append(md5Results[i].ToString("x2"));
+                }
+            }
+            return hash.ToString();
         }
     }
 }
