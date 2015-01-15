45eb889bca619a06f10aa73bfb330afa705121d8
mklitgaard <mtklitgaard@gmail.com>
2015-01-14 20:25:00 -0700

    Add md5 test

---
+++ ComicLookup/ComicLookup.Services.Tests/MarvelKeyRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace ComicLookup.Services.Tests
+{
+    public class MarvelKeyRepositoryTests
+    {
+        private MarvelKeyRepository _classUnderTest;
+        private string _timeStamp;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _timeStamp = "timestamp";
+            _classUnderTest = new MarvelKeyRepository(_timeStamp);
+        }
+
+        public class Hash : MarvelKeyRepositoryTests
+        {
+            [Test]
+            public void ReturnsCorrectHash()
+            {
+                var expected = "EXPECTEDHASH";
+
+                var actual = _classUnderTest.Hash();
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+
+        }
+    }
+}
